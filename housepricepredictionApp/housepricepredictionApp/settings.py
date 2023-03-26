f0d0f891754198eb10ecafc94e1be67f5fe0bf31
Phoowainyein <phoo.nyein@student.samk.fi>
2023-03-26 17:06:01 +0300

    Add changes to settings and templates

--- housepricepredictionApp/housepricepredictionApp/settings.py
+++ housepricepredictionApp/housepricepredictionApp/settings.py
@@ -125,8 +125,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'lookup/static')]
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
